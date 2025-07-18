0954c29c3cc0b7fa11903f0b27e486f05325204f
Maksym <simbanyan98@gmail.com>
2025-07-18 13:19:20 +0200

    Database connection fix

--- app-next/utils/api.js
+++ app-next/utils/api.js
@@ -1,3 +1,6 @@
 export default function api(route) {
-    return `${process.env.NEXT_PUBLIC_API_URL}/api${route}`;
+  const baseUrl =
+    process.env.NEXT_PUBLIC_API_URL ||
+    "https://meal-sharing-api-wpfg.onrender.com";
+  return `${baseUrl}/api${route}`;
 }
